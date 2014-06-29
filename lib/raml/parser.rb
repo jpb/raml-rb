f9825fddb3e6cbd539ac40eab851b73605032c70
James Brennan <james@jamesbrennan.ca>
2014-06-28 20:51:19 -0700

    Handle traits

--- lib/raml/parser.rb
+++ lib/raml/parser.rb
@@ -2,10 +2,11 @@ require 'yaml'
 
 module Raml
   class Parser
-    attr_accessor :yaml, :root
+    attr_accessor :yaml, :root, :traits
 
     def initialize(yaml)
       @yaml = YAML.load(yaml)
+      @traits = {}
     end
 
     def parse
@@ -31,6 +32,8 @@ module Raml
         case key
         when *Root::ATTRIBUTES
           root.send("#{key}=".to_sym, parse_value(value))
+        when 'traits'
+          parse_traits(parse_value(value))
         when /^\//
           root.resources << parse_resource(key, parse_value(value))
         else
@@ -38,13 +41,29 @@ module Raml
         end
       end
 
+      def parse_traits(traits)
+        traits.each do |name, data|
+          @traits[name] = data
+        end
+      end
+
       def parse_resource(parent, uri_partial, data)
         resource = Resource.new(parent, uri_partial)
+        parse_resource_attributes(resource, data)
+      end
 
+      def parse_resource_attributes(resource, data)
         data.each do |key, value|
           case key
           when *Resource::ATTRIBUTES
             resource.send("#{key}=".to_sym, parse_value(value))
+          when 'is'
+            value = value.is_a?(Array) ? value : [value]
+            value.each do |name|
+              unless traits[name].nil?
+                resource = parse_resource_attributes(resource, traits[name])
+              end
+            end
           when /^\//
             resource.resources << parse_resource(resource, key, parse_value(value))
           when *%w(get put post delete)
@@ -59,11 +78,21 @@ module Raml
 
       def parse_method(action, data)
         method = Method.new(action)
+        parse_method_attributes(method, data)
+      end
 
+      def parse_method_attributes(method, data)
         data.each do |key, value|
           case key
           when *Method::ATTRIBUTES
             method.send("#{key}=".to_sym, parse_value(value))
+          when 'is'
+            value = value.is_a?(Array) ? value : [value]
+            value.each do |name|
+              unless traits[name].nil?
+                method = parse_method_attributes(method, traits[name])
+              end
+            end
           else
             raise UnknownAttributeError.new "Unknown method key: #{key}"
           end
@@ -74,11 +103,21 @@ module Raml
 
       def parse_response(code, data)
         response = Response.new(code)
+        parse_response_attributes(response, data)
+      end
 
+      def parse_response_attributes(response, data)
         data.each do |key, value|
           case key
           when *Response::ATTRIBUTES
             response.send("#{key}=".to_sym, parse_value(value))
+          when 'is'
+            value = value.is_a?(Array) ? value : [value]
+            value.each do |name|
+              unless traits[name].nil?
+                response = parse_response_attributes(response, traits[name])
+              end
+            end
           when 'body'
             parse_value(value).each do |type, data|
               response.bodies << parse_body(type, data)
@@ -93,11 +132,21 @@ module Raml
 
       def parse_body(type, data)
         body = Body.new(type)
+        parse_body_attributes(body, data)
+      end
 
+      def parse_body_attributes(body, data)
         data.each do |key, value|
           case key
           when *Response::ATTRIBUTES
-            response.send("#{key}=".to_sym, parse_value(value))
+            body.send("#{key}=".to_sym, parse_value(value))
+          when 'is'
+            value = value.is_a?(Array) ? value : [value]
+            value.each do |name|
+              unless traits[name].nil?
+                body = parse_body_attributes(body, traits[name])
+              end
+            end
           else
             raise UnknownAttributeError.new "Unknown response key: #{key}"
           end
--- raml-rb.gemspec
+++ raml-rb.gemspec
@@ -1,15 +1,14 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'raml-rb/version'
+require 'raml/version'
 
 Gem::Specification.new do |spec|
   spec.name          = 'raml-rb'
-  spec.version       = Raml::Rb::VERSION
+  spec.version       = Raml::VERSION
   spec.authors       = ['James Brennan']
   spec.email         = ['james@jamesbrennan.ca']
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{A RAML parser implemented in Ruby}
   spec.homepage      = ''
   spec.license       = 'MIT'
 
@@ -20,4 +19,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.6'
   spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rspec', '~> 3.0.0'
 end
