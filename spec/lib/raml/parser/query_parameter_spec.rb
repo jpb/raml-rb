154326ca57214ebc6044ea58908dacf79fd9dec3
James Brennan <james@jamesbrennan.ca>
2014-07-03 21:51:48 -0700

    Added query parameter spec

---
+++ spec/lib/raml/parser/query_parameter_spec.rb
@@ -0,0 +1,24 @@
+require 'raml/parser/query_parameter'
+
+describe Raml::Parser::QueryParameter do
+  let(:parent) { double() }
+  let(:instance) { Raml::Parser::QueryParameter.new(parent) }
+  let(:type) { 'application/json' }
+  let(:attribute) { { 'description' => 'dogs', 'type' => 'cats', 'example' => 'birds' } }
+
+  describe '#parse' do
+    subject { instance.parse(type, attribute) }
+
+    it { should be_kind_of Raml::QueryParameter }
+    its(:description) { should == 'dogs' }
+    its(:type) { should == 'cats' }
+    its(:example) { should == 'birds' }
+  end
+
+  describe '#query_parameter' do
+    before { instance.parse(type, attribute) }
+    subject { instance.query_parameter }
+    it { should be_kind_of Raml::QueryParameter }
+  end
+
+end
