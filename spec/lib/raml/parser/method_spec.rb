f15c8d97d0fba730d394ed3be49fb68349f9c6e0
James Brennan <james@jamesbrennan.ca>
2014-07-03 21:48:54 -0700

    Test traits

--- spec/lib/raml/parser/method_spec.rb
+++ spec/lib/raml/parser/method_spec.rb
@@ -3,20 +3,32 @@ require 'raml/parser/method'
 describe Raml::Parser::Method do
 
   describe '#parse' do
-    let(:attribute) { { 'description' => 'The description', 'headers' => [{ 'key' => 'value' }], 'responses' => ['cats'], 'query_parameters' => ['dogs'] } }
-    let(:parent) { double(traits: { 'with_content' => { 'title' => 'Trait title', 'content' => 'Trait content' } }, trait_names: nil) }
+    let(:attributes) { { 'description' => 'The description', 'headers' => [{ 'key' => 'value' }], 'responses' => ['cats'], 'query_parameters' => ['dogs'] } }
+    let(:parent) { double(traits: { 'cats' => { 'description' => 'Trait description', 'headers' => { 'trait_key' => 'trait_value' } } }, trait_names: nil) }
     before do
       Raml::Parser::Response.any_instance.stub(:parse).and_return('cats')
       Raml::Parser::QueryParameter.any_instance.stub(:parse).and_return('dogs')
     end
-    subject { Raml::Parser::Method.new(parent).parse('get', attribute) }
+    subject { Raml::Parser::Method.new(parent).parse('get', attributes) }
 
     it { should be_kind_of Raml::Method }
     its(:method) { should == 'get' }
     its(:description) { should == 'The description' }
-    its(:headers) { should == [ 'key' => 'value' ] }
+    its(:headers) { should == [ { 'key' => 'value' } ] }
     its(:responses) { should == ['cats'] }
     its(:query_parameters) { should == ['dogs'] }
+
+    context 'with trait' do
+      let(:attributes) { { 'is' => [ 'cats' ], 'responses' => ['cats'], 'query_parameters' => ['dogs'] } }
+      its(:description) { should == 'Trait description' }
+      its(:headers) { [ { 'trait_key' => 'trait_value' } ] }
+    end
+
+    context 'override trait attributes' do
+      let(:attributes) { { 'is' => [ 'cats' ], 'description' => 'The description', 'headers' => [{ 'key' => 'value' }], 'responses' => ['cats'], 'query_parameters' => ['dogs'] } }
+      its(:description) { should == 'The description' }
+      its(:headers) { should == [ { 'key' => 'value' } ] }
+    end
 
   end
 
